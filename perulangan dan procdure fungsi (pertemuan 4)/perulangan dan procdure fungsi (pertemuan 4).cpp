6f039fcca6002abaf2929642f2e57e20c97da883
dannyfadli <danyfdly95@gmail.com>
2024-03-19 10:23:24 +0700

    menambahkan  jumlah perulangan for yang sudah ditentukan <5

--- perulangan dan procdure fungsi (pertemuan 4)/perulangan dan procdure fungsi (pertemuan 4).cpp
+++ perulangan dan procdure fungsi (pertemuan 4)/perulangan dan procdure fungsi (pertemuan 4).cpp
@@ -12,5 +12,14 @@ int main()
 
     cout << " Nilai i terakhir : " << i << endl;
 
+    for (i = 0; i < 5; i++) {
+        cout << "Masukan nilai index ke-" << i << " :";
+        cin >> arr[1];
+
+    }
+    
+
 }
+
+
 
